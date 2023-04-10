93ac36f03e19ea856d0434bb0d733e25b4983def
moradan <realmoradan@hotmail.com>
2023-04-10 17:40:02 -0300

    primer js

--- js/01-intro.js
+++ js/01-intro.js
@@ -1,1 +1,6 @@
-console.log("Hola mundo! desde archivo externo.");
+let anioNacimiento = parseInt(prompt("Ingrese su año de nacimiento: "));
+let fecha = new Date();
+let anioActual = fecha.getFullYear;
+let edad = anioActual - anioNacimiento;
+
+console.log(`Su edad es de ${edad-1} ó ${edad}.`);
