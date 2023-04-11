22260d33536cdc22a00c5a3074af7062960b0e93
moradan <realmoradan@hotmail.com>
2023-04-11 16:32:48 -0300

    primer navbar

--- js/01-intro.js
+++
@@ -1,6 +0,0 @@
-let anioNacimiento = parseInt(prompt("Ingrese su año de nacimiento: "));
-let fecha = new Date();
-let anioActual = fecha.getFullYear;
-let edad = anioActual - anioNacimiento;
-
-console.log(`Su edad es de ${edad-1} ó ${edad}.`);
