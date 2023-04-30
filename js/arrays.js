8740b6daa17d0eaa7393f1c9b9da75445c2cd75a
moradan <realmoradan@hotmail.com>
2023-04-30 12:36:20 -0300

    completado hasta ejercicio 7 de arrays

--- js/arrays.js
+++ js/arrays.js
@@ -36,6 +36,52 @@ function continuar(mensaje) {
     return (continuar == "S");
 }
 
+// pide una cantidad dada de numeros al usuario, devuelve un array no vacio con los numeros ingresados. Si el usuario, en cualquier momento, ingresa un dato que no es un número o preciona cancelar la función devuelve null
+function pedirNumeros(cantidad) {
+    let numeros = [];
+    
+    for (let i = 0; i < cantidad; i++) {
+        let respuesta = parseInt(prompt(`Ingrese un numero (${i + 1} de ${cantidad}).`));
+        if (isNaN(respuesta)) {
+            throw new Error("No se ingresó un número valido o se precionó cancelar.");
+        } else {
+            numeros.push(respuesta);
+        }
+    }
+    return numeros;
+}
+
+// pide una cantidad de caracteres, devuelve un array no vacio con los caracteres ingresados. Si el usuario en cualquier momento preciona cancelar o la tecla ESC devuelve null
+function pedirCaracteres(cantidad) {
+    let caracteres = [];
+    
+    for (let i = 0; i < cantidad; i++) {
+        let caracter = prompt(`Ingrese un caracter (${i + 1} de ${cantidad}).`);
+        if (caracter == null) {
+            throw new Error("No se ingresaron caracteres o bien se presionó cancel.");
+        } else {
+            caracteres.push(caracter);
+        }
+    }
+    return caracteres;
+}
+
+// recibe un array y suma todos los valores numericos salteandose una cantidad de elementos de acuerdo al parametro SALTO. Ignora valores no numericos. Devuelve null si no recibe un array con al menos 1 elemento. Devuelve 0 si recibe un array con contenido pero sin numeros.
+function sumarArray(array) {
+    let suma = 0;
+    if (array == null || array == undefined || array.length==0) {
+        throw new Error("El array no es válido.");
+    } else {
+        for(const elemento of array) {
+            if (!isNaN(elemento)) {
+                suma += elemento;
+            }
+        }
+        return suma;
+    }
+}
+
+/***************************************************************************************************** */
 // Ejercicio 1 Arriba del Promedio
 // pide una lista de 10 numeros, calcula el promedio, y luego muestra ese promedio y lista los numero por arriba del promedio.
 function arribaDelPromedio () {
@@ -57,67 +103,40 @@ function arribaDelPromedio () {
     }
 }
 
-// pide una cantidad dada de numeros al usuario, devuelve un array no vacio con los numeros ingresados. Si el usuario, en cualquier momento, ingresa un dato que no es un número o preciona cancelar la función devuelve null
-function pedirNumeros(cantidad) {
-    let numeros = [];
-    
-    for (let i = 0; i < cantidad; i++) {
-        let respuesta = parseInt(prompt(`Ingrese un numero (${i + 1} de ${cantidad}).`));
-        if (isNaN(respuesta)) {
-            return null;
-        } else {
-            numeros.push(respuesta);
-        }
-    }
-    return numeros;
-}
-
 // recibe un array de numeros, calcula el promedio y lo devuelve
 function calcularPromedio(numeros) {
     return sumarArray(numeros) / numeros.length;
 }
 
-// recibe un array y suma todos los valores numericos, ignora los valores que no sean numericos. Devuelve null si no recibe un array con al menos 1 elemento. Devuelve 0 si recibe un array con contenido pero sin numeros.
-function sumarArray(array) {
-    let suma = 0;
-    if (array == null || array == undefined || array.length==0) {
-        return null;
-    } else {
-        for(const elemento of array) {
-            if (!isNaN(elemento)) {
-                suma += elemento;
-            }
-        }
-        return suma;
-    }
-}
 
-// mostrar todos los múltiplos del último número en un array de 10.
+/***************************************************************************************************** */
+// Ejercicio 2 mostrar todos los múltiplos del último número en un array de 10.
 function multiplosDelUltimo () {
-    let numeros = [];
-    numeros = pedirNumeros(CANTIDAD_NUMEROS);
-    if(numeros == null) {
+    try {
+        const numeros = pedirNumeros(CANTIDAD_NUMEROS);
+        const ultimo = numeros.pop();
+        const multiplos = multiplosDe(numeros, ultimo);
+        
+        if (multiplos.length > 0) {
+            mostrar(`El último número de la lista es el ${ultimo}\nLos siguientes son todos los números de la lista que son múltiplos de ${ultimo}`);
+            mostrarArray(multiplos);
+        } else {
+            avisar("Ningún número de la lista es múltiplo del último.");
+        }
+    } catch {
         avisar(CANCELADO);
-        return -1;
     }
 
-    let ultimo = numeros.pop();
-    const multiplos = multiplosDe(numeros, ultimo);
-
-    if (multiplos != null && multiplos.length > 0) {
-        mostrar(`El último número de la lista es el ${ultimo}\nLos siguientes son todos los números de la lista que son múltiplos de ${ultimo}`);
-        mostrarArray(multiplos);
-    } else {
-        avisar("Ningún número de la lista es múltiplo del último.");
-    }
 }
 
 // devuelve un array multiplos que solo contiene aquellos elementos del array lista que son multiplos del numero indicado.
 function multiplosDe(lista, numero) {
 
     const multiplos = [];
-    if (lista.length == 0 || isNaN(numero)){
-        return null;
+    if (lista.length == 0){
+        throw new Error("Lista vacía.");
+    } else if (isNaN(numero)) {
+        throw new Error("El número provisto no es válido.");
     } else {
         for (const elemento of lista) {
             if (esMultiplo(elemento, numero)) {
@@ -128,20 +147,134 @@ function multiplosDe(lista, numero) {
     }
 }
 
-// ejercicio que pide 10 numeros, calcula el maximo, muestra ese maximo e indica cuantas veces fue ingresado
+
+/***************************************************************************************************** */
+// ejercicio 3 pide 10 numeros, calcula el maximo, muestra ese maximo e indica cuantas veces fue ingresado
 function frecuenciaDelMaximo () {
-    let numeros = [];
-    numeros = pedirNumeros(CANTIDAD_NUMEROS);
-    if(numeros == null) {
+    
+    try {
+        let numeros = pedirNumeros(CANTIDAD_NUMEROS);
+        let maximo = buscarMaximo(numeros);
+        let cantidad = calcularFrecuencia(maximo, numeros);
+    
+        mostrar(`El máximo número de la lista es el ${maximo}, y fue ingresado ${cantidad} veces.`);
+        return cantidad;
+    } catch {
+        avisar(CANCELADO);
+        return -1;
+    }
+}
+
+
+/***************************************************************************************************** */
+// ejercicio 4 que pide 10 numeros y suma los que se hayan ingresado en los turnos pares
+function programaSumarLugaresPares() {
+    try {
+        let numeros = pedirNumeros(CANTIDAD_NUMEROS);
+        let suma = sumarLugaresPares(numeros);
+        mostrar(suma);
+    } catch {
         avisar(CANCELADO);
         return -1;
     }
 
-    // en un contexto diferente debería validar que máximo no esté undefined, sin embargo en este algoritmo no es necesario. buscarMaximo siempre devuelve el número máximo contenido en el array de números, y devuelve null si el array no contiene ningún número, si el array es null o si el array está vacío. Acá el array es la variable numeros y no puede ser null porque ese caso ya está contemplado en su validación, y no puede contener caracteres no numéricos ni estar vaciós porque lo estamos obteniendo a traves de la funcion pedirNumeros que devuelve null en ambos casos.
-    let maximo = buscarMaximo(numeros);
-    let cantidad = calcularFrecuencia(maximo, numeros);
+}
 
-    mostrar(`El máximo número de la lista es el ${maximo}, y fue ingresado ${cantidad} veces.`);
-    return cantidad;
+// suma todos los elementos de lugares pares en un array ignorando datos no numericos.
+function sumarLugaresPares(array) {
+    let suma = 0;
+
+    if (array == null || array == undefined || array.length==0) {
+        return null;
+    } else {
+        for (let i = 1; i < array.length; i += 2) {
+            if (!isNaN(array[i])) {
+                suma += array[i];
+            }
+        }
+        return suma;
+    }
+}
+
+
+/***************************************************************************************************** */
+// ejercicio 5 tomar un array de 9 caracteres e invertir el orden
+function invertirArrayDeCaracteres() {
+    try {
+        let caracteres = pedirCaracteres(CANTIDAD_CARACTERES);
+        invertirArray(caracteres);
+        mostrar(caracteres);
+    } catch {
+        avisar(CANCELADO);
+        return -1;
+    }
+
+}
+
+function invertirArray(array){
+    if (array == null || array == undefined) {
+        return null;
+    } else {
+        let longitud = array.length;
+        for (let i = 0; i < Math.floor(longitud / 2); i++) {
+            let a = array[i];
+            let b = array[longitud - 1 - i];
+            array[i] = b;
+            array[longitud - 1 - i] = a;
+        }
+    }
+}
+
+
+/***************************************************************************************************** */
+// ejercicio 6 desplazar un array de 9 caracteres 1 lugar hacia la derecha y ponerl el ultimo al principio.
+function rightShift() {
+    try {
+        let caracteres = pedirCaracteres(CANTIDAD_CARACTERES);
+        let ultimo = caracteres.pop();
+        caracteres.unshift(ultimo);
+        mostrar(caracteres);
+    } catch {
+        avisar(CANCELADO);
+        return -1;
+    }
+}
+
+
+/***************************************************************************************************** */
+// ejercicio 7 determinar si la palabra representada por los caracteres de un array conforman un palindromo
+function programaEsPalindromo() {
+    try {
+        let caracteres = pedirCaracteres(CANTIDAD_CARACTERES);
+        mostrar(esPalindromo(caracteres));
+        return esPalindromo(caracteres);
+    } catch {
+        avisar(CANCELADO);
+        return -1;
+    }
+}
+
+function esPalindromo(array) {
+    const LONGITUD = array.length;
+    const ULTIMO = LONGITUD - 1;
+
+    if (array == null || array == undefined || LONGITUD == 0) {
+        return null;
+    }
+
+    let i = 0
+    const CENTRO = Math.floor(LONGITUD / 2); 
+    while (i < CENTRO && array[i] == array[ULTIMO - i]) {
+        i++;
+    }
+
+    return i == CENTRO;
+}
+
+
+/***************************************************************************************************** */
+// ejercicio 8 toma un array y muestra sus elementos sin repetir elementos
+function mostrarSinRepetidos() {
+    
 }
 
